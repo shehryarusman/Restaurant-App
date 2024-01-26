fa17dfd948998d8dfc257d4ab679ce07a3c2fea1
John Khachian <jaksawesome10@gmail.com>
2024-01-26 17:44:21 -0500

    swiping cards partly work

--- src/screens/swipe/SwipeScreen.js
+++ src/screens/swipe/SwipeScreen.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Appearance, View, StyleSheet } from "react-native";
+import Swiper from 'react-native-deck-swiper';
+// Constants
+import { colors } from "@Junto/constants";
 // Components
 import { ScreenContainer, Header } from "@Junto/fundamentals";
-import { Button } from "@Junto/elements";
+import { Text } from "@Junto/elements";
 
 const SwipeScreen = () => {
+    const theme = Appearance.getColorScheme();
+    const cuisines = [
+        "American",
+        "Chinese",
+        "French",
+        "Greek",
+        "Indian",
+        "Italian",
+        "Japanese",
+        "Korean",
+        "Mexican",
+        "Thai",
+        "Vietnamese"
+    ];
+    const [preferredQuisines, setPreferredQuisines] = useState([]);
+
+    const addPreferredCuisine = (cuisine) => {
+        setPreferredQuisines([
+            ...preferredQuisines,
+            cuisine
+        ]);
+    };
+
+    const getReccomendedRestaurants = () => {
+        console.log("Getting reccomended restaurants for");
+        console.log(preferredQuisines.join(", "));
+    };
+
     return (
         <ScreenContainer>
             <Header
@@ -12,13 +43,26 @@ const SwipeScreen = () => {
                 hideLeftIcon
             />
             <View style={styles.container}>
-                <Button
-                    title="Let them cook"
-                    style={{ marginBottom: 25 }}
-                />
-                <Button
-                    title="Let me cook"
-                />
+                <Swiper
+                    cards={cuisines}
+                    renderCard={(card) => {
+                        return (
+                            <View>
+                                <Text style={styles.text}>{card}</Text>
+                            </View>
+                        )
+                    }}
+                    onSwipedRight={(index, name) => addPreferredCuisine(name)}
+                    onSwipedAll={getReccomendedRestaurants}
+                    cardIndex={0}
+                    stackSize={3}
+                    cardVerticalMargin={20}
+                    cardStyle={{...styles.card, ...themeStyles[theme].card}}
+                    backgroundColor="transparent"
+                    horizontalSwipe
+                    verticalSwipe={false}
+                >
+                </Swiper>
             </View>
         </ScreenContainer>
     );
@@ -27,9 +71,35 @@ const SwipeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: 20,
-        justifyContent: "center"
+        height: "50%"
     },
-});
+    card: {
+        flex: 1,
+        borderRadius: 15,
+        borderWidth: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%"
+    },
+    text: {
+      textAlign: "center",
+      fontSize: 50
+    }
+  });
+
+const themeStyles = {
+    light: StyleSheet.create({
+        card: {
+            backgroundColor: colors.WHITE,
+            borderColor: colors.light.FIRST
+        }
+    }),
+    dark: StyleSheet.create({
+        card: {
+            backgroundColor: colors.dark.SECOND,
+            borderColor: colors.dark.FOURTH
+        }
+    })
+};
 
 export default SwipeScreen;

7efc9b47245a04df61d9d0f820309181ba90dd04
John Khachian <jaksawesome10@gmail.com>
2024-02-09 13:56:40 -0500

    fixed spelling for Alyque

--- src/screens/swipe/SwipeScreen.js
+++ src/screens/swipe/SwipeScreen.js
@@ -6,34 +6,46 @@ import { colors } from "@Junto/constants";
 // Components
 import { ScreenContainer, Header } from "@Junto/fundamentals";
 import { Text } from "@Junto/elements";
+import { LoadingAnimation } from "@Junto/animations";
+// API
+import JuntoApi from "../../api/Junto";
 
 const SwipeScreen = () => {
     const theme = Appearance.getColorScheme();
     const cuisines = [
-        { name: "American", source: require("../../../assets/quisine-stock-photos/american.jpeg") },
-        { name: "Chinese", source: require("../../../assets/quisine-stock-photos/chinese.jpeg") },
-        { name: "French", source: require("../../../assets/quisine-stock-photos/french.jpeg") },
-        { name: "Greek", source: require("../../../assets/quisine-stock-photos/greek.jpeg") },
-        { name: "Indian", source: require("../../../assets/quisine-stock-photos/indian.jpeg") },
-        { name: "Italian", source: require("../../../assets/quisine-stock-photos/italian.jpeg") },
-        { name: "Japanese", source: require("../../../assets/quisine-stock-photos/japanese.jpeg") },
-        { name: "Korean", source: require("../../../assets/quisine-stock-photos/korean.jpeg") },
-        { name: "Mexican", source: require("../../../assets/quisine-stock-photos/mexican.jpeg") },
-        { name: "Thai", source: require("../../../assets/quisine-stock-photos/thai.jpeg") },
-        { name: "Vietnamese", source: require("../../../assets/quisine-stock-photos/vietnamese.jpeg") }
+        { name: "American", source: require("../../../assets/cuisine-stock-photos/american.jpeg") },
+        { name: "Chinese", source: require("../../../assets/cuisine-stock-photos/chinese.jpeg") },
+        { name: "French", source: require("../../../assets/cuisine-stock-photos/french.jpeg") },
+        { name: "Greek", source: require("../../../assets/cuisine-stock-photos/greek.jpeg") },
+        { name: "Indian", source: require("../../../assets/cuisine-stock-photos/indian.jpeg") },
+        { name: "Italian", source: require("../../../assets/cuisine-stock-photos/italian.jpeg") },
+        { name: "Japanese", source: require("../../../assets/cuisine-stock-photos/japanese.jpeg") },
+        { name: "Korean", source: require("../../../assets/cuisine-stock-photos/korean.jpeg") },
+        { name: "Mexican", source: require("../../../assets/cuisine-stock-photos/mexican.jpeg") },
+        { name: "Thai", source: require("../../../assets/cuisine-stock-photos/thai.jpeg") },
+        { name: "Vietnamese", source: require("../../../assets/cuisine-stock-photos/vietnamese.jpeg") }
     ];
     const [preferredQuisines, setPreferredQuisines] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const addPreferredCuisine = (cuisine) => {
         setPreferredQuisines([
             ...preferredQuisines,
-            cuisine
+            cuisine.name
         ]);
     };
 
-    const getReccomendedRestaurants = () => {
+    const getReccomendedRestaurants = async () => {
         console.log("Getting reccomended restaurants for");
         console.log(preferredQuisines.join(", "));
+        setLoading(true);
+        const { data } = await JuntoApi.get("/");
+        const dummyData = {
+            dishes: [],
+            restaurants: []
+        };
+        setLoading(false);
+        console.log(data);
     };
 
     return (
@@ -43,30 +55,36 @@ const SwipeScreen = () => {
                 hideLeftIcon
             />
             <View style={styles.container}>
-                <Swiper
-                    cards={cuisines}
-                    renderCard={({ name, source }) => {
-                        return (
-                            <View>
-                                <Text style={styles.text}>{name}</Text>
-                                <Image
-                                    source={source}
-                                    style={styles.image}
-                                ></Image>
-                            </View>
-                        )
-                    }}
-                    onSwipedRight={(index, name) => addPreferredCuisine(name)}
-                    onSwipedAll={getReccomendedRestaurants}
-                    cardIndex={0}
-                    stackSize={3}
-                    cardVerticalMargin={20}
-                    cardStyle={{...styles.card, ...themeStyles[theme].card}}
-                    backgroundColor="transparent"
-                    horizontalSwipe
-                    verticalSwipe={false}
-                >
-                </Swiper>
+                {
+                    !loading ? (
+                        <Swiper
+                            cards={cuisines}
+                            renderCard={({ name, source }) => {
+                                return (
+                                    <View>
+                                        <Text style={styles.text}>{name}</Text>
+                                        <Image
+                                            source={source}
+                                            style={styles.image}
+                                        ></Image>
+                                    </View>
+                                )
+                            }}
+                            onSwipedRight={(index, name) => addPreferredCuisine(name)}
+                            onSwipedAll={getReccomendedRestaurants}
+                            cardIndex={0}
+                            stackSize={3}
+                            cardVerticalMargin={20}
+                            cardStyle={{...styles.card, ...themeStyles[theme].card}}
+                            backgroundColor="transparent"
+                            horizontalSwipe
+                            verticalSwipe={false}
+                        >
+                        </Swiper>
+                    ) : (
+                        <LoadingAnimation />
+                    )
+                }
             </View>
         </ScreenContainer>
     );

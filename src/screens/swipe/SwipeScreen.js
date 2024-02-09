507f89374f894911fda493a4de47cdf9f7d6ea4b
John Khachian <jaksawesome10@gmail.com>
2024-02-09 13:10:26 -0500

    got the pictures working for the cuisine selection

--- src/screens/swipe/SwipeScreen.js
+++ src/screens/swipe/SwipeScreen.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Appearance, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Appearance, View, Image, StyleSheet } from "react-native";
 import Swiper from 'react-native-deck-swiper';
 // Constants
 import { colors } from "@Junto/constants";
@@ -10,17 +10,17 @@ import { Text } from "@Junto/elements";
 const SwipeScreen = () => {
     const theme = Appearance.getColorScheme();
     const cuisines = [
-        "American",
-        "Chinese",
-        "French",
-        "Greek",
-        "Indian",
-        "Italian",
-        "Japanese",
-        "Korean",
-        "Mexican",
-        "Thai",
-        "Vietnamese"
+        { name: "American", source: require("../../../assets/quisine-stock-photos/american.jpeg") },
+        { name: "Chinese", source: require("../../../assets/quisine-stock-photos/chinese.jpeg") },
+        { name: "French", source: require("../../../assets/quisine-stock-photos/french.jpeg") },
+        { name: "Greek", source: require("../../../assets/quisine-stock-photos/greek.jpeg") },
+        { name: "Indian", source: require("../../../assets/quisine-stock-photos/indian.jpeg") },
+        { name: "Italian", source: require("../../../assets/quisine-stock-photos/italian.jpeg") },
+        { name: "Japanese", source: require("../../../assets/quisine-stock-photos/japanese.jpeg") },
+        { name: "Korean", source: require("../../../assets/quisine-stock-photos/korean.jpeg") },
+        { name: "Mexican", source: require("../../../assets/quisine-stock-photos/mexican.jpeg") },
+        { name: "Thai", source: require("../../../assets/quisine-stock-photos/thai.jpeg") },
+        { name: "Vietnamese", source: require("../../../assets/quisine-stock-photos/vietnamese.jpeg") }
     ];
     const [preferredQuisines, setPreferredQuisines] = useState([]);
 
@@ -45,10 +45,14 @@ const SwipeScreen = () => {
             <View style={styles.container}>
                 <Swiper
                     cards={cuisines}
-                    renderCard={(card) => {
+                    renderCard={({ name, source }) => {
                         return (
                             <View>
-                                <Text style={styles.text}>{card}</Text>
+                                <Text style={styles.text}>{name}</Text>
+                                <Image
+                                    source={source}
+                                    style={styles.image}
+                                ></Image>
                             </View>
                         )
                     }}
@@ -71,19 +75,24 @@ const SwipeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        height: "50%"
     },
     card: {
         flex: 1,
+        flexDirection: "column",
         borderRadius: 15,
         borderWidth: 2,
         alignItems: "center",
-        justifyContent: "center",
-        height: "100%"
+        backgroundColor: "green",
+        height: "90%",
+        overflow: "hidden"
     },
     text: {
       textAlign: "center",
-      fontSize: 50
+      fontSize: 50,
+      margin: 15
+    },
+    image: {
+        flex: 1
     }
   });
 

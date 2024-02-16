015f793279bde64da0eb5ab5e0e5dcd855c8d894
John Khachian <jaksawesome10@gmail.com>
2024-02-16 16:45:28 -0500

    Added grid after selection

--- src/screens/swipe/SwipeScreen.js
+++ src/screens/swipe/SwipeScreen.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Appearance, View, Image, StyleSheet } from "react-native";
+import { Appearance, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Swiper from 'react-native-deck-swiper';
 // Constants
 import { colors } from "@Junto/constants";
+import { capitalize } from "@Junto/utils";
 // Components
 import { ScreenContainer, Header } from "@Junto/fundamentals";
 import { Text } from "@Junto/elements";
@@ -12,40 +13,57 @@ import JuntoApi from "../../api/Junto";
 
 const SwipeScreen = () => {
     const theme = Appearance.getColorScheme();
-    const cuisines = [
-        { name: "American", source: require("../../../assets/cuisine-stock-photos/american.jpeg") },
-        { name: "Chinese", source: require("../../../assets/cuisine-stock-photos/chinese.jpeg") },
-        { name: "French", source: require("../../../assets/cuisine-stock-photos/french.jpeg") },
-        { name: "Greek", source: require("../../../assets/cuisine-stock-photos/greek.jpeg") },
-        { name: "Indian", source: require("../../../assets/cuisine-stock-photos/indian.jpeg") },
-        { name: "Italian", source: require("../../../assets/cuisine-stock-photos/italian.jpeg") },
-        { name: "Japanese", source: require("../../../assets/cuisine-stock-photos/japanese.jpeg") },
-        { name: "Korean", source: require("../../../assets/cuisine-stock-photos/korean.jpeg") },
-        { name: "Mexican", source: require("../../../assets/cuisine-stock-photos/mexican.jpeg") },
-        { name: "Thai", source: require("../../../assets/cuisine-stock-photos/thai.jpeg") },
-        { name: "Vietnamese", source: require("../../../assets/cuisine-stock-photos/vietnamese.jpeg") }
-    ];
-    const [preferredQuisines, setPreferredQuisines] = useState([]);
+    const [cards, setCards] = useState([
+        { text: "american", source: require("../../../assets/cuisine-stock-photos/american.jpeg") },
+        { text: "chinese", source: require("../../../assets/cuisine-stock-photos/chinese.jpeg") },
+        { text: "french", source: require("../../../assets/cuisine-stock-photos/french.jpeg") },
+        { text: "greek", source: require("../../../assets/cuisine-stock-photos/greek.jpeg") },
+        { text: "indian", source: require("../../../assets/cuisine-stock-photos/indian.jpeg") },
+        { text: "italian", source: require("../../../assets/cuisine-stock-photos/italian.jpeg") },
+        { text: "japanese", source: require("../../../assets/cuisine-stock-photos/japanese.jpeg") },
+        { text: "korean", source: require("../../../assets/cuisine-stock-photos/korean.jpeg") },
+        { text: "mexican", source: require("../../../assets/cuisine-stock-photos/mexican.jpeg") },
+        { text: "thai", source: require("../../../assets/cuisine-stock-photos/thai.jpeg") },
+        { text: "vietnamese", source: require("../../../assets/cuisine-stock-photos/vietnamese.jpeg") }
+    ]);
+    /* Selection levels:
+        0 -> Selecting cuisines
+        1 -> Selecting dishes
+        2 -> Show results (no more cards)
+    */
+    const [selectionLevel, setSelectionLevel] = useState(0);
+    const [acceptedCards, setAcceptedCards] = useState([]);
+    const [results, setResults] = useState([]);
+    const [expandedDish, setExpandedDish] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const addPreferredCuisine = (cuisine) => {
-        setPreferredQuisines([
-            ...preferredQuisines,
-            cuisine.name
+    const addPreferredCuisine = (card) => {
+        setAcceptedCards([
+            ...acceptedCards,
+            card
         ]);
     };
 
-    const getReccomendedRestaurants = async () => {
-        console.log("Getting reccomended restaurants for");
-        console.log(preferredQuisines.join(", "));
+    const onSwipedAll = async () => {
         setLoading(true);
-        const { data } = await JuntoApi.get("/");
-        const dummyData = {
-            dishes: [],
-            restaurants: []
-        };
+        switch(selectionLevel){
+            case 0:
+                const preferredQuisines = acceptedCards.map(({ text }) => text);
+                const { data: dishes } = await JuntoApi.post("/recs", { preferredQuisines });
+                
+                setCards(dishes.map(dish => ({ ...dish, text: dish.name })));
+                setSelectionLevel(1);
+                
+                break;
+            case 1:
+                setResults(acceptedCards);
+                setSelectionLevel(2);
+                
+            default:
+                break;
+        }
+        setAcceptedCards([]);
         setLoading(false);
-        console.log(data);
     };
 
     return (
@@ -57,30 +75,54 @@ const SwipeScreen = () => {
             <View style={styles.container}>
                 {
                     !loading ? (
-                        <Swiper
-                            cards={cuisines}
-                            renderCard={({ name, source }) => {
-                                return (
-                                    <View>
-                                        <Text style={styles.text}>{name}</Text>
-                                        <Image
-                                            source={source}
-                                            style={styles.image}
-                                        ></Image>
-                                    </View>
-                                )
-                            }}
-                            onSwipedRight={(index, name) => addPreferredCuisine(name)}
-                            onSwipedAll={getReccomendedRestaurants}
-                            cardIndex={0}
-                            stackSize={3}
-                            cardVerticalMargin={20}
-                            cardStyle={{...styles.card, ...themeStyles[theme].card}}
-                            backgroundColor="transparent"
-                            horizontalSwipe
-                            verticalSwipe={false}
-                        >
-                        </Swiper>
+                            selectionLevel === 2 ? (
+                                <View style={styles.resultsList}>
+                                    {
+                                        results.map((dish) => (
+                                            <TouchableOpacity
+                                                style={styles.resultsListItem}
+                                                key={dish.id}
+                                                onPress={() => {
+                                                    setExpandedDish(expandedDish !== dish.id ? dish.id : null);
+                                                }}
+                                            >
+                                                <Image
+                                                    source={require("../../../assets/cuisine-stock-photos/american.jpeg")}
+                                                    style={{
+                                                        ...styles.resultsListItemImage,
+                                                        ...((expandedDish === dish.id) ?  styles.resultsListItemExpandedImage : {})
+                                                    }}
+                                                ></Image>
+                                            </TouchableOpacity>
+                                        ))
+                                    }
+                                </View>
+                            ) : (
+                                <Swiper
+                                    cards={cards}
+                                    renderCard={({ text, source }) => {
+                                        return (
+                                            <View>
+                                                <Text style={styles.text}>{capitalize(text)}</Text>
+                                                <Image
+                                                    source={source}
+                                                    style={styles.image}
+                                                ></Image>
+                                            </View>
+                                        )
+                                    }}
+                                    onSwipedRight={(index, name) => addPreferredCuisine(name)}
+                                    onSwipedAll={onSwipedAll}
+                                    cardIndex={0}
+                                    stackSize={3}
+                                    cardVerticalMargin={20}
+                                    cardStyle={{...styles.card, ...themeStyles[theme].card}}
+                                    backgroundColor="transparent"
+                                    horizontalSwipe
+                                    verticalSwipe={false}
+                                >
+                                </Swiper>
+                            )
                     ) : (
                         <LoadingAnimation />
                     )
@@ -111,6 +153,25 @@ const styles = StyleSheet.create({
     },
     image: {
         flex: 1
+    },
+    resultsHeader: {
+        fontSize: 30
+    },
+    resultsList: {
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    resultsListItem: {
+        margin: 10
+    },
+    resultsListItemImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 15
+    },
+    resultsListItemExpandedImage: {
+        width: 200,
+        height: 200
     }
   });
 
@@ -119,10 +180,18 @@ const themeStyles = {
         card: {
             backgroundColor: colors.WHITE,
             borderColor: colors.light.FIRST
+        },
+        resultsListItem: {
+            backgroundColor: colors.WHITE,
+            borderColor: colors.light.FIRST
         }
     }),
     dark: StyleSheet.create({
         card: {
+            backgroundColor: colors.dark.SECOND,
+            borderColor: colors.dark.FOURTH
+        },
+        resultsListItem: {
             backgroundColor: colors.dark.SECOND,
             borderColor: colors.dark.FOURTH
         }
---
+++ src/utils/utils.js
@@ -0,0 +1,6 @@
+
+const capitalize = s => s.substring(0,1).toUpperCase() + s.substring(1);
+
+module.exports = {
+    capitalize
+};
